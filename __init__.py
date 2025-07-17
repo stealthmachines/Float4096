aadcf6697d84ebb339e2ab69675b5aa4f9fb923c
Josef Kulovany <stealthmachines@gmail.com>
2025-07-17 10:59:07 -0600

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .float4096 import Float4096 as float4096, Float4096Array, GRAElement, sqrt, exp, log, log10, cos, pi, inv, linspace, logspace, mean, stddev, percentile, max, min, abs, D, invert_D
