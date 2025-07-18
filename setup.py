0f354c54acebb9c3ae2b4bfe968b58112e7a5fbf
Josef Kulovany <stealthmachines@gmail.com>
2025-07-18 00:59:14 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-# setup.py
-
-from setuptools import setup, find_packages
+# project_root/setup.py
+from setuptools import setup
 
 setup(
     name='float4096',
     version='0.7.0',
-    packages=find_packages(),
+    packages=['float4096'],  # Only include float4096 package
     install_requires=[
-        'numpy>=1.20.0',
-        'sympy>=1.9',
+        'numpy>=1.21.0',
+        'sympy>=1.8',
         'scipy>=1.7.0',
+        'pandas>=1.3.0',
+        'matplotlib>=3.4.0',
+        'tqdm>=4.60.0',
+        'joblib>=1.0.0',
         'base4096 @ git+https://github.com/ZCHGorg/base4096.git',
+        'pyfftw>=0.13.0',  # Optional, but include for consistency
     ],
     author='Josef Kulovany',
     author_email='stealthmachines@gmail.com',
@@ -23,6 +27,6 @@ setup(
         'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     license='Proprietary',
 )
