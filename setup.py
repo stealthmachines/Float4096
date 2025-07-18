eaf7cdf4bd78061baed941ba2eb008c46ee75674
Josef Kulovany <stealthmachines@gmail.com>
2025-07-17 20:14:08 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,19 @@ setup(
         'numpy>=1.20.0',
         'sympy>=1.9',
         'scipy>=1.7.0',
-        'base4096',
+        'base4096',  # Ensure this is pip-installable or bundled
     ],
     author='Josef Kulovany',
     author_email='stealthmachines@gmail.com',
     description='High-precision base4096 arithmetic with FFT multiplication, cubic spline, and complex support',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/stealthmachines/Float4096',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: ATTACHED',
+        'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    license='Proprietary',
 )
